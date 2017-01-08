970471e162d50fcd253f272744850247055430d9
Stephane de Labrusse <stephdl@de-labrusse.fr>
2017-01-09 00:13:45 +0100

    devel

--- root/usr/share/nethesis/NethServer/Module/ClamScan.php
+++ root/usr/share/nethesis/NethServer/Module/ClamScan.php
@@ -20,6 +20,12 @@ class ClamScan extends \Nethgui\Controller\AbstractController
     public function initialize()
     {
         parent::initialize();
+        $weekdays = $this->createValidator()->memberOf(array('1d','2d','3d',
+                '4d','5d','6d','7d'));
+        $hour = $this->createValidator()->memberOf(array('1h','2h','3h',
+                '4h','5h','6h','7h','8h','9h','10h',
+                        '11h','12h','13h','14h','15h','16h','17h',
+                                '18h','19h','20h','21h','22h','23h'));
 
         $this->declareParameter('BlockEncrypted', Validate::SERVICESTATUS, array('configuration', 'clamscan', 'BlockEncrypted'));
         $this->declareParameter('Bytecode', Validate::SERVICESTATUS, array('configuration', 'clamscan', 'Bytecode'));
@@ -39,48 +45,48 @@ class ClamScan extends \Nethgui\Controller\AbstractController
         $this->declareParameter('ScanPe', Validate::SERVICESTATUS, array('configuration', 'clamscan', 'ScanPe'));
         $this->declareParameter('status', Validate::SERVICESTATUS, array('configuration', 'clamscan', 'status'));
         $this->declareParameter('reallyWantToMove', Validate::SERVICESTATUS, array('configuration', 'clamscan', 'reallyWantToMove'));
-        $this->declareParameter('JobHour', Validate::NONNEGATIVE_INTEGER, array('configuration', 'clamscan', 'JobHour'));
-        $this->declareParameter('JobDay', Validate::POSITIVE_INTEGER, array('configuration', 'clamscan', 'JobDay'));
+        $this->declareParameter('JobHour', $hour, array('configuration', 'clamscan', 'JobHour'));
+        $this->declareParameter('JobDay', $weekday, array('configuration', 'clamscan', 'JobDay'));
     }
 
     public function prepareView(\Nethgui\View\ViewInterface $view)
     {
         parent::prepareView($view);
         $view['JobDayDatasource'] = \Nethgui\Renderer\AbstractRenderer::hashToDatasource(array(
-                '1' => $view->translate('MONDAY'),
-                '2' => $view->translate('TUESDAY'),
-                '3' => $view->translate('WEDNESDAY'),
-                '4' => $view->translate('THURSDAY'),
-                '5' => $view->translate('FRIDAY'),
-                '6' => $view->translate('SATURDAY'),
-                '7' => $view->translate('SUNDAY'),
+                '1d' => $view->translate('MONDAY'),
+                '2d' => $view->translate('TUESDAY'),
+                '3d' => $view->translate('WEDNESDAY'),
+                '4d' => $view->translate('THURSDAY'),
+                '5d' => $view->translate('FRIDAY'),
+                '6d' => $view->translate('SATURDAY'),
+                '7d' => $view->translate('SUNDAY'),
         ));
 
         $view['JobHourDatasource'] = \Nethgui\Renderer\AbstractRenderer::hashToDatasource(array(
-                '0' => $view->translate('${0} Hour', array(0)),
-                '1' => $view->translate('${0} Hour', array(1)),
-                '2' => $view->translate('${0} Hour', array(2)),
-                '3' => $view->translate('${0} Hour', array(3)),
-                '4' => $view->translate('${0} Hour', array(4)),
-                '5' => $view->translate('${0} Hour', array(5)),
-                '6' => $view->translate('${0} Hour', array(6)),
-                '7' => $view->translate('${0} Hour', array(7)),
-                '8' => $view->translate('${0} Hour', array(8)),
-                '9' => $view->translate('${0} Hour', array(9)),
-                '10' => $view->translate('${0} Hour', array(10)),
-                '11' => $view->translate('${0} Hour', array(11)),
-                '12' => $view->translate('${0} Hour', array(12)),
-                '13' => $view->translate('${0} Hour', array(13)),
-                '14' => $view->translate('${0} Hour', array(14)),
-                '15' => $view->translate('${0} Hour', array(15)),
-                '16' => $view->translate('${0} Hour', array(16)),
-                '17' => $view->translate('${0} Hour', array(17)),
-                '18' => $view->translate('${0} Hour', array(18)),
-                '19' => $view->translate('${0} Hour', array(19)),
-                '20' => $view->translate('${0} Hour', array(20)),
-                '21' => $view->translate('${0} Hour', array(21)),
-                '22' => $view->translate('${0} Hour', array(22)),
-                '23' => $view->translate('${0} Hour', array(23)),
+                '0h' => $view->translate('${0} Hour', array(0)),
+                '1h' => $view->translate('${0} Hour', array(1)),
+                '2h' => $view->translate('${0} Hour', array(2)),
+                '3h' => $view->translate('${0} Hour', array(3)),
+                '4h' => $view->translate('${0} Hour', array(4)),
+                '5h' => $view->translate('${0} Hour', array(5)),
+                '6h' => $view->translate('${0} Hour', array(6)),
+                '7h' => $view->translate('${0} Hour', array(7)),
+                '8h' => $view->translate('${0} Hour', array(8)),
+                '9h' => $view->translate('${0} Hour', array(9)),
+                '10h' => $view->translate('${0} Hour', array(10)),
+                '11h' => $view->translate('${0} Hour', array(11)),
+                '12h' => $view->translate('${0} Hour', array(12)),
+                '13h' => $view->translate('${0} Hour', array(13)),
+                '14h' => $view->translate('${0} Hour', array(14)),
+                '15h' => $view->translate('${0} Hour', array(15)),
+                '16h' => $view->translate('${0} Hour', array(16)),
+                '17h' => $view->translate('${0} Hour', array(17)),
+                '18h' => $view->translate('${0} Hour', array(18)),
+                '19h' => $view->translate('${0} Hour', array(19)),
+                '20h' => $view->translate('${0} Hour', array(20)),
+                '21h' => $view->translate('${0} Hour', array(21)),
+                '22h' => $view->translate('${0} Hour', array(22)),
+                '23h' => $view->translate('${0} Hour', array(23)),
         ));
     }
 
